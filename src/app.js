8b4d5ef54d76bcd1c26ef3baad51556001fb2ab8
Olga Sokolianska <99847102+Sokolianska@users.noreply.github.com>
2022-06-14 16:16:43 +0200

    temperature link

--- src/app.js
+++ src/app.js
@@ -25,6 +25,8 @@ function displayTemperature(response) {
     let dateElement = document.querySelector("#date");
     let iconElement = document.querySelector("#icon");
 
+    celsiusTemperature = response.data.main.temp;
+
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
@@ -35,6 +37,7 @@ function displayTemperature(response) {
     iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
+//searching
 function search(city) {
     let apiKey = "e0dced0781a18aa3906255142be14578";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
@@ -51,4 +54,37 @@ function handleSubmit(event) {
 search("New York");
 
 let form = document.querySelector("#search-form");
-form.addEventListener("submit",handleSubmit);
+form.addEventListener("submit", handleSubmit);
+
+//fahrenheit - link
+function displayFahrenheitTemperature(event) {
+    event.preventDefault();
+    let temperatureElement = document.querySelector("#temperature");
+    //remove the active link
+    celsiusLink.classList.remove("active");
+    fahrenheitLink.classList.add("active");
+
+    let fahrenheitTemperature = (celsiusTemperature*9)/5+32;
+    
+    temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+//celsius-link
+function displayCelsiusTemperature(event) {
+    event.preventDefault();
+    //remove the active link
+    celsiusLink.classList.add("active");
+    fahrenheitLink.classList.remove("active");
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
+    
+}
+
+let celsiusTemperature = null;
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
