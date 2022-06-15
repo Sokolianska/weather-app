c23b29795d14c07b0217e389380513ae9352e109
Olga Sokolianska <99847102+Sokolianska@users.noreply.github.com>
2022-06-15 15:17:44 +0200

    dinamic HTML in JS code for Forecast

--- src/app.js
+++ src/app.js
@@ -88,3 +88,26 @@ let celsiusTemperature = null;
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
+//forecast
+function displayForecast(){
+    let forecastElement = document.querySelector("#forecast");
+
+    let forecastHTML = `<div class="row">`;
+    let days = ["Thu", "Fri", "Sat", "Sun"];
+    days.forEach(function(day){
+    forecastHTML =
+        forecastHTML +
+        `<div class="col-2">
+    <div class="weather-forecast-date">
+    ${day}</div>
+    <img src="https://ssl.gstatic.com/onebox/weather/48/partly_cloudy.png" alt=""width="42">
+    <div class="weather-forecast-temperatures">
+    <span class="weather-forecast-temperature-max">18°</span>
+    <span class="weather-forecast-temperature-min">12°</span>
+    </div></div>`;
+        })
+    forecastHTML = forecastHTML + `</div>`;
+    forecastElement.innerHTML = forecastHTML;
+}
+
+displayForecast();
